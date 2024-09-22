0ed56eba108263dc2decc48142bc59c77e0062fc
jihun5914 <128763317+jihun5914@users.noreply.github.com>
2024-09-22 20:49:28 +0900

    Add files via upload

---
+++ Java project 1/Hello/src/stduy01/Helloworld.java
@@ -0,0 +1,12 @@
+package stduy01;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Java project 1/OpenChallenge1/src/MyInFoApp/MyInFoAp.java
@@ -0,0 +1,13 @@
+package MyInFoApp;
+
+public class MyInFoAp {
+
+	public static void main(String[] args) {
+		System.out.println("KIMJIHUN");
+		System.out.println("25 years old");
+		System.out.println("Department of Computer software");
+		// TODO Auto-generated method stub
+
+	}
+
+}
